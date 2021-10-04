1a4654f314270210fb2c2d0638584e87de710130
DESKTOP-M11KKJ7\JonBe <jberkness16@winona.edu>
2021-10-04 15:01:48 -0500

    singwithoptions

--- src/com/Assignment4/ApplicationRunnable.java
+++ src/com/Assignment4/ApplicationRunnable.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 
 public class ApplicationRunnable {
 	UserService userService = new UserService();
+
 	public static void main(String[] args) {
 		
 		
@@ -18,16 +19,45 @@ public class ApplicationRunnable {
 		
 		for (int i = 0; i < 20; i++) {
 			users[i] = userService.createUser("user" + (i+1), "password" + (i+1), )
-			static Scanner scanner = new Scanner(System.in);
-			System.out.println("To login enter your username");
-			String username = scanner.next();
-			System.out.println("Enter your password");
-			String password = scanner.next();
-			System.out.println("Welcome" + name);
-		}
-		System.out.println(users);
-		
+
+	static Scanner scanner = new Scanner(System.in);System.out.println("To login enter your username");
+	String username = scanner.next();System.out.println("Enter your password");
+	String password = scanner.next();System.out.println("Welcome"+name);}System.out.println(users);
 
 	}
+
+	
+	
+	
+	
+	
+	
+	// once logged in
+
+	While(option)
+
+	option 0
+	Log in
+	as another
+	user System.out.println("Which user would you like to log in as? (Type a valid username)");"Username not found, enter a valid username."
+
+	option 1
+	Update username
+	String updateUsername = scanner.nextLine();
+
+	option 2
+	Update password
+	String updatePassword = scanner.nextLine();
+
+	option 3
+	Update name
+	String updateName = scanner.nextLine();
+
+	option 4
+	Exit String = scanner.nextLine();
+
+	if option 0
+
+	System.out.println("Which user would you like to login as? (Type in a valid username)"
 
 }
